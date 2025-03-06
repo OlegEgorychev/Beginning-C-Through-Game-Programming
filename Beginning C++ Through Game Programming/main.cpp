01921f429a045a73745c2c172245dcae8993c6bf
Oleg Egorychev <egorychevoleg92@gmail.com>
2025-03-06 11:18:36 +0400

    add programm: String Tester

--- Beginning C++ Through Game Programming/main.cpp
+++ Beginning C++ Through Game Programming/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <iomanip>
 #include <cstdlib>
 #include <ctime>
@@ -153,7 +154,7 @@ void task3()
 			cout << "Invalid input! Please enter 1, 2 or 3: ";
 			cin >> answer;
 		}
-		
+
 
 
 		cout << "\n";
@@ -173,55 +174,100 @@ void task3()
 			break;
 		}
 	} while (minValue <= maxValue);
-		
-			cout << "\nSomething went wrong. Did you change your number?\n\n";
+
+	cout << "\nSomething went wrong. Did you change your number?\n\n";
 }
 
 void task4()
 {
 	cout << "\nCounting forward: \n";
-		for (int i = 0; i < 10; ++i)
-			{
-				cout << i << " ";
-			}
+	for (int i = 0; i < 10; ++i)
+	{
+		cout << i << " ";
+	}
 
 	cout << "\n\nCounting backward: \n";
-		for (int i = 9; i >= 0; --i)
-			{		
-				cout << i << " ";
-			}
+	for (int i = 9; i >= 0; --i)
+	{
+		cout << i << " ";
+	}
 
 	cout << "\n\nCounting by fives: \n";
-		for (int i = 0; i <= 50; i += 5)
-			{
-				cout << i << " ";
-			}
+	for (int i = 0; i <= 50; i += 5)
+	{
+		cout << i << " ";
+	}
 	cout << "\n\nCounting with null statements: \n";
 
-		int count = 0;
+	int count = 0;
 
-		for (; count < 10;)
-			{
-				cout << count << " ";
-				++count;
-			}
+	for (; count < 10;)
+	{
+		cout << count << " ";
+		++count;
+	}
 
 	cout << "\n\nCounting with nested for loops: \n";
-		int ROWS = 5;
-		int COLUMNS = 3;
+	int ROWS = 5;
+	int COLUMNS = 3;
 
-		for (int i = 0; i < ROWS; ++i)
+	for (int i = 0; i < ROWS; ++i)
+	{
+		for (int j = 0; j < COLUMNS; ++j)
 		{
-			for (int j = 0; j < COLUMNS; ++j)
-			{
-				cout << i << "." << j << " ";
-			}
-			cout << endl;
+			cout << i << "." << j << " ";
 		}
-
 		cout << endl;
+	}
 
-		
+	cout << endl;
+
+
+}
+
+void task5()
+{
+	string word1 = "Game";
+	string word2("Over");
+	string word3(3, '!');
+	string phrase = word1 + " " + word2 + word3;
+
+	cout << "The phrase is: " << phrase << "\n\n";
+	cout << "The phrase has: " << phrase.size() << " characters in it.\n\n";
+	cout << "The character at position 0 is: " << phrase[0] << "\n\n";
+	cout << "Changing the character at position 0.\n";
+
+	phrase[0] = 'L';
+
+	cout << "The phrase is now: " << phrase << "\n\n";
+
+	for (unsigned int i = 0; i < phrase.size(); ++i)
+	{
+		cout << "Character at position " << i << " is: " << phrase[i] << endl;
+	}
+
+	cout << "\nThe sequence 'Over' begins at location ";
+	cout << phrase.find('Over') << endl;
+
+	if (phrase.find("eggplant") == string::npos)
+	{
+		cout << "'eggplant' is not in the phrase.\n\n";
+	}
+
+	phrase.erase(4, 5);
+	cout << "The phrase is now: " << phrase << endl;
+	
+	phrase.erase(4);
+	cout << "The phrase is now: " << phrase << endl;
+
+	phrase.erase();
+	cout << "The phrase is now: " << phrase << endl;
+
+	if (phrase.empty())
+	{
+		cout << "\nThe phrase is no more.\n\n";
+	}
+
 }
 
 int main()
@@ -237,6 +283,7 @@ int main()
 		cout << "\n2. Programm: Menu Chooser";
 		cout << "\n3. Game: guess MY number";
 		cout << "\n4. Programm: Counter";
+		cout << "\n5. Programm: String Tester";
 
 		cout << "\n\nEnter number to chose game: ";
 
@@ -251,6 +298,8 @@ int main()
 		case 3: task3();
 			break;
 		case 4: task4();
+			break;
+		case 5: task5();
 			break;
 
 		case 0: cout << "\nExit\n\n";
